400da2d32ce8f9709ccc17dc062a97678bbd3557
Marcin <Marcin.koz52@gmail.com>
2023-09-08 15:03:46 +0200

    Dodanie prostej animacji dla projectCard

--- portfolio/src/components/Main/containers/Projects/ProjectCard.jsx
+++ portfolio/src/components/Main/containers/Projects/ProjectCard.jsx
@@ -1,5 +1,6 @@
 import { Card, CardContent, CardHeader, Typography } from "@mui/material";
 import SwipeableTextMobileStepper from "../../../custom/AutoPlaySwipeableViews ";
+import { useEffect, useRef, useState } from "react";
 const ProjectCard = ({alignItems}) => {
     const header = "Delivery Driver";
     const description = "Moja fascynacja programowaniem zaczęła się, gdy po raz pierwszy zetknąłem się z C#. Ta wszechstronna i potężna technologia zapoczątkowała moją przygodę z .NET, a od tego czasu nie patrzyłem już wstecz. Pracując w różnych projektach, od małych startupów po duże przedsiębiorstwa, zdobyłem cenne doświadczenie w projektowaniu, rozwijaniu i utrzymaniu aplikacji na platformie .NET.";
@@ -27,14 +28,38 @@ const ProjectCard = ({alignItems}) => {
             'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
         },
       ];
+    const projectCardRef = useRef(null);
 
-    return (<Card className="projectCard">
+    const [scroll, setScroll] = useState(false);
+    useEffect(() => {
+      let projectCardRefCurrent = projectCardRef.current;
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            setScroll(true); // Element ".Card" jest widoczny na ekranie
+            observer.disconnect(); // Przestań obserwować po znalezieniu elementu
+          }
+        });
+      });
+  
+      if (projectCardRefCurrent) {
+        observer.observe(projectCardRefCurrent);
+      }
+  
+      return () => {
+        if (projectCardRefCurrent) {
+          observer.unobserve(projectCardRefCurrent); // Upewnij się, że przerywasz obserwację, gdy komponent jest odmontowywany
+        }
+      };
+    }, []);
+
+    return (
+      <Card ref={projectCardRef} className={scroll ? "projectCard showOnPage" : "projectCard"}>
         <CardHeader className={cardHeaderclass} title={header}/>
         <CardContent className="projectCardContent">
             <SwipeableTextMobileStepper className={carouselClass} images={images} showControls={false} showTitle={false} />
             <Typography className="cardDescription">{description}</Typography>
         </CardContent>
-         
-    </Card>)
-}
+    </Card>
+      )}
 export default ProjectCard;
