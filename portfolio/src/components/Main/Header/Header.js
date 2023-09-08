1b0e5dc2822c19ffefcf41d875da1ddba7102752
Marcin <Marcin.koz52@gmail.com>
2023-09-08 15:03:54 +0200

    Ustawienie górnej nawigacji na sticky

--- portfolio/src/components/Main/Header/Header.js
+++ portfolio/src/components/Main/Header/Header.js
@@ -1,4 +1,4 @@
-import { AppBar, Box, IconButton, Toolbar, Typography, MenuItem, Menu, Button } from '@mui/material';
+import { AppBar, IconButton, Toolbar, Typography, MenuItem, Menu, Button } from '@mui/material';
 import { scroller } from 'react-scroll';
 import MenuIcon from '@mui/icons-material/Menu';
 import React from 'react';
@@ -36,52 +36,50 @@ const Header = ({fetchMoreData, componentsNames}) => {
     };
 
 return (
-    <Box>
-        <AppBar position="static" className='header'>
-            <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    Xenorsek!
-                </Typography>
-                <div className='iconNavigation'>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        aria-controls={open ? 'basic-menu' : undefined}
-                        aria-haspopup="true"
-                        aria-expanded={open ? 'true' : undefined}
-                        onClick={handleClick}
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                </div>
-                <Menu
-                    id="basic-menu"
-                    anchorEl={anchorEl}
-                    open={open}
-                    onClose={handleClose}
-                    MenuListProps={{
-                    'aria-labelledby': 'basic-button',
-                    }}
+    <AppBar position="sticky" className='header'>
+        <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Xenorsek!
+            </Typography>
+            <div className='iconNavigation'>
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    aria-controls={open ? 'basic-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                    onClick={handleClick}
+                    sx={{ mr: 2 }}
                 >
-                    <MenuItem onClick={()=>{scrollToSection(0)}}>O mnie</MenuItem>                
-                    <MenuItem onClick={()=>{scrollToSection(1)}}>Umiejętności</MenuItem>
-                    <MenuItem onClick={()=>{scrollToSection(2)}}>Projekty</MenuItem>
-                    <MenuItem onClick={()=>{scrollToSection(3)}}>Doświadczenie</MenuItem>
-                    <MenuItem onClick={()=>{scrollToSection(4)}}>Kontakt</MenuItem>
-                </Menu>
-                <div className="navigation">
-                    <Button color="secondary" variant="text" onClick={()=>{scrollToSection(0)}}>O mnie</Button>                
-                    <Button color="secondary" variant="text" onClick={()=>{scrollToSection(1)}}>Umiejętności</Button>
-                    <Button color="secondary" variant="text" onClick={()=>{scrollToSection(2)}}>Projekty</Button>
-                    <Button color="secondary" variant="text" onClick={()=>{scrollToSection(3)}}>Doświadczenie</Button>
-                    <Button color="secondary" variant="text" onClick={()=>{scrollToSection(4)}}>Kontakt</Button>
-                </div>
-            </Toolbar>
-        </AppBar>
-    </Box>
+                    <MenuIcon />
+                </IconButton>
+            </div>
+            <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                'aria-labelledby': 'basic-button',
+                }}
+            >
+                <MenuItem onClick={()=>{scrollToSection(0)}}>O mnie</MenuItem>                
+                <MenuItem onClick={()=>{scrollToSection(1)}}>Umiejętności</MenuItem>
+                <MenuItem onClick={()=>{scrollToSection(2)}}>Projekty</MenuItem>
+                <MenuItem onClick={()=>{scrollToSection(3)}}>Doświadczenie</MenuItem>
+                <MenuItem onClick={()=>{scrollToSection(4)}}>Kontakt</MenuItem>
+            </Menu>
+            <div className="navigation">
+                <Button color="secondary" variant="text" onClick={()=>{scrollToSection(0)}}>O mnie</Button>                
+                <Button color="secondary" variant="text" onClick={()=>{scrollToSection(1)}}>Umiejętności</Button>
+                <Button color="secondary" variant="text" onClick={()=>{scrollToSection(2)}}>Projekty</Button>
+                <Button color="secondary" variant="text" onClick={()=>{scrollToSection(3)}}>Doświadczenie</Button>
+                <Button color="secondary" variant="text" onClick={()=>{scrollToSection(4)}}>Kontakt</Button>
+            </div>
+        </Toolbar>
+    </AppBar>
 )
 }
 export default Header;
