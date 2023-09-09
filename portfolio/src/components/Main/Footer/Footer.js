17f8ffdd98307415fcf0821377f47392e39913a4
Marcin <Marcin.koz52@gmail.com>
2023-09-09 11:03:19 +0200

    utworzenie styli dla experience i footer

--- portfolio/src/components/Main/Footer/Footer.js
+++ portfolio/src/components/Main/Footer/Footer.js
@@ -2,16 +2,13 @@ import { Card, CardContent, CardHeader, Link, Typography } from "@mui/material";
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 function Footer(){
     return(
-        <div className="footer">
-            <Card>
+        <Card className="footer">
             <CardHeader title="Kontakt"/>   
             <CardContent>
                 Jeśli jesteś zainteresowany współpracą lub masz pytania, śmiało się ze mną skontaktuj. Jesteś serdecznie zaproszony do odwiedzenia mojego portfolio, gdzie znajdziesz więcej informacji o mojej pracy i projektach.
             <Typography><Link href="" ><LinkedInIcon/></Link></Typography>
             </CardContent>
-            </Card>
-            
-        </div>
+        </Card>
     )
 }
 export default Footer;
--- portfolio/src/components/Main/containers/Experience/Experience.jsx
+++ portfolio/src/components/Main/containers/Experience/Experience.jsx
@@ -2,16 +2,14 @@ import { Card, CardContent, CardHeader, Typography } from "@mui/material";
 
 function Experience(){
     return (
-        <div className="About">
-            <Card>
-                <CardHeader title="Moje Doświadczenie"/>            
-                <CardContent>
-                    <Typography> 
-                    W mojej karierze miałem przyjemność pracować nad różnorodnymi projektami. Niezależnie od skali projektu, zawsze staram się dostarczyć rozwiązania, które przynoszą wartość i zadowolenie klientom. Kilka z moich projektów można zobaczyć w sekcji portfolio.
-                    </Typography> 
-                </CardContent>
-            </Card>
-        </div>
+        <Card className="experience">
+            <CardHeader title="Moje Doświadczenie"/>            
+            <CardContent>
+                <Typography> 
+                W mojej karierze miałem przyjemność pracować nad różnorodnymi projektami. Niezależnie od skali projektu, zawsze staram się dostarczyć rozwiązania, które przynoszą wartość i zadowolenie klientom. Kilka z moich projektów można zobaczyć w sekcji portfolio.
+                </Typography> 
+            </CardContent>
+        </Card>
     )
 }
 
