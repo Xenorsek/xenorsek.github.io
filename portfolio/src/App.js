35ab2dfefcf7ba442fa16a401cf2d1735d2cbd6f
Marcin <Marcin.koz52@gmail.com>
2023-09-09 22:07:24 +0200

    Dodanie toggle na color mode

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,18 +1,38 @@
 import './assets/styles/main.scss'
 import Main from './components/Main/Main';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import React from 'react';
+import React, { useEffect } from 'react';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import CssBaseline from '@mui/material/CssBaseline';
 
+export const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+
 function App() {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const selectedDarkMode = JSON.parse(localStorage.getItem("darkmode"));
+  const [mode, setMode] = React.useState(selectedDarkMode != null ? selectedDarkMode : prefersDarkMode);
+
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === false ? true : false));
+      },
+    }),
+    [],
+  );
+
+  useEffect(()=>{
+    var darkmode = localStorage.getItem("darkmode");
+    if(!darkmode){
+      localStorage.setItem("darkmode", mode);
+    }
+  })
 
   const theme = React.useMemo(
     () =>
       createTheme({
         palette: {
-          mode: prefersDarkMode ? 'dark' : 'light',
+          mode: mode ? 'dark' : 'light',
           primary: {
             light: '#e2f5f8',
             main: '#258ab3',
@@ -27,17 +47,16 @@ function App() {
           }
         },
       }),
-    [prefersDarkMode],
+    [mode],
   );
 
   return (
-    <>
+    <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-         <Main />
+        <Main />
       </ThemeProvider>
-      
-    </>
+    </ColorModeContext.Provider>
   );
 }
 
--- portfolio/src/components/Main/Header/Header.js
+++ portfolio/src/components/Main/Header/Header.js
@@ -1,10 +1,25 @@
-import { AppBar, IconButton, Toolbar, Typography, MenuItem, Menu, Button } from '@mui/material';
+import { AppBar, IconButton, Toolbar, Typography, MenuItem, Menu, Button, Switch, useMediaQuery } from '@mui/material';
 import { scroller } from 'react-scroll';
 import MenuIcon from '@mui/icons-material/Menu';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { ColorModeContext } from '../../../App';
 const Header = ({fetchMoreData, componentsNames}) => {
+    const [darkMode, setDarkMode] = useState(false);
+    const colorMode = React.useContext(ColorModeContext);
+    const preferDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+    
+    useEffect(()=>{
+        const darkModeFromStorage = JSON.parse(localStorage.getItem("darkmode"));
+        if(darkModeFromStorage == null){
+            setDarkMode(preferDarkMode)
+        }
+        else{
+            setDarkMode(darkModeFromStorage);
+        }
+    },[preferDarkMode])
+
     const scrollToSection = (section) => {
-        setAnchorEl(null);
+        setAnchorElNavigation(null);
         if(componentsNames[section].visible){
             scrollToElement(componentsNames[section].name)
         }
@@ -25,15 +40,34 @@ const Header = ({fetchMoreData, componentsNames}) => {
             offset: -offset
         });
     }
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
+
+    const [anchorElNavigation, setAnchorElNavigation] = React.useState(null);
+    const [anchorElPageSettings, setAnchorElPageSettings] = React.useState(null);
+    const openPageSettings = Boolean(anchorElPageSettings);
+    const openPageNavigation = Boolean(anchorElNavigation);
+
+    const handleOpenNavigation = (event) => {
+        setAnchorElNavigation(event.currentTarget);
     };
 
-    const handleClose = () => {
-        setAnchorEl(null);
+    const handleCloseNavigation = () => {
+        setAnchorElNavigation(null);
     };
+
+    const handleOpenPageSettings = (event) =>{
+        setAnchorElPageSettings(event.currentTarget);
+    }
+
+    const handleClosePageSettings = () => {
+        setAnchorElPageSettings(null);
+    };
+
+    const handleChangeDarkMode = (event) => {
+        setDarkMode(event.target.checked);
+        colorMode.toggleColorMode();
+        localStorage.setItem("darkmode", event.target.checked);
+
+    }
 
 return (
     <AppBar position="sticky" className='header'>
@@ -44,41 +78,63 @@ return (
             <div className='iconNavigation'>
                 <IconButton
                     size="large"
-                    edge="start"
                     color="inherit"
                     aria-label="menu"
-                    aria-controls={open ? 'basic-menu' : undefined}
+                    aria-controls={openPageNavigation ? 'navigation-menu' : undefined}
                     aria-haspopup="true"
-                    aria-expanded={open ? 'true' : undefined}
-                    onClick={handleClick}
+                    aria-expanded={openPageNavigation ? 'true' : undefined}
+                    onClick={handleOpenNavigation}
                     sx={{ mr: 2 }}
                 >
                     <MenuIcon />
                 </IconButton>
             </div>
-            <Menu
-                id="basic-menu"
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                MenuListProps={{
-                'aria-labelledby': 'basic-button',
-                }}
-            >
-                <MenuItem onClick={()=>{scrollToSection(0)}}>O mnie</MenuItem>                
-                <MenuItem onClick={()=>{scrollToSection(1)}}>Umiejętności</MenuItem>
-                <MenuItem onClick={()=>{scrollToSection(2)}}>Projekty</MenuItem>
-                <MenuItem onClick={()=>{scrollToSection(3)}}>Doświadczenie</MenuItem>
-                <MenuItem onClick={()=>{scrollToSection(4)}}>Kontakt</MenuItem>
-            </Menu>
             <div className="navigation">
                 <Button color="secondary" variant="text" onClick={()=>{scrollToSection(0)}}>O mnie</Button>                
                 <Button color="secondary" variant="text" onClick={()=>{scrollToSection(1)}}>Umiejętności</Button>
                 <Button color="secondary" variant="text" onClick={()=>{scrollToSection(2)}}>Projekty</Button>
                 <Button color="secondary" variant="text" onClick={()=>{scrollToSection(3)}}>Doświadczenie</Button>
                 <Button color="secondary" variant="text" onClick={()=>{scrollToSection(4)}}>Kontakt</Button>
+                <IconButton 
+                    size="large"
+                    edge="end"
+                    color="inherit"
+                    aria-label="menu"
+                    aria-controls={openPageSettings ? 'page-settings-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={openPageSettings ? 'true' : undefined}
+                    onClick={handleOpenPageSettings}
+                    sx={{ mr: 2 }}>
+                    <MenuIcon />
+                </IconButton>
             </div>
         </Toolbar>
+        <Menu
+            id="navigation-menu"
+            anchorEl={anchorElNavigation}
+            open={openPageNavigation}
+            onClose={handleCloseNavigation}
+            MenuListProps={{
+            'aria-labelledby': 'basic-button',
+            }}
+        >
+            <MenuItem onClick={()=>{scrollToSection(0)}}>O mnie</MenuItem>                
+            <MenuItem onClick={()=>{scrollToSection(1)}}>Umiejętności</MenuItem>
+            <MenuItem onClick={()=>{scrollToSection(2)}}>Projekty</MenuItem>
+            <MenuItem onClick={()=>{scrollToSection(3)}}>Doświadczenie</MenuItem>
+            <MenuItem onClick={()=>{scrollToSection(4)}}>Kontakt</MenuItem>
+        </Menu>
+        <Menu
+            id="page-settings-menu"
+            anchorEl={anchorElPageSettings}
+            open={openPageSettings}
+            onClose={handleClosePageSettings}
+            MenuListProps={{
+            'aria-labelledby': 'basic-button',
+            }}
+        >
+            <MenuItem>Dark mode <Switch checked={darkMode} onChange={handleChangeDarkMode}/></MenuItem>                
+        </Menu>
     </AppBar>
 )
 }
