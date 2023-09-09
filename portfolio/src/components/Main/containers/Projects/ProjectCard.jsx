f479b8c8e8d63f9850d03dad57036d074a66b3d4
Marcin <Marcin.koz52@gmail.com>
2023-09-09 11:24:13 +0200

    Dodanie zakładki na repository w projectCard

--- portfolio/src/components/Main/containers/Projects/ProjectCard.jsx
+++ portfolio/src/components/Main/containers/Projects/ProjectCard.jsx
@@ -1,11 +1,13 @@
-import { Card, CardContent, CardHeader, Typography } from "@mui/material";
+import { Card, CardActions, CardContent, CardHeader, Collapse, IconButton, Link, Typography } from "@mui/material";
 import SwipeableTextMobileStepper from "../../../custom/AutoPlaySwipeableViews ";
 import { useEffect, useRef, useState } from "react";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 const ProjectCard = ({alignItems}) => {
     const header = "Delivery Driver";
     const description = "Moja fascynacja programowaniem zaczęła się, gdy po raz pierwszy zetknąłem się z C#. Ta wszechstronna i potężna technologia zapoczątkowała moją przygodę z .NET, a od tego czasu nie patrzyłem już wstecz. Pracując w różnych projektach, od małych startupów po duże przedsiębiorstwa, zdobyłem cenne doświadczenie w projektowaniu, rozwijaniu i utrzymaniu aplikacji na platformie .NET.";
     const carouselClass = "projectCardCarousel " + alignItems;
-    const cardHeaderclass = "cardHeader " + alignItems;
+    const cardHeaderClass = "cardHeader " + alignItems;
+    const collapseClass = "collapseCard " + alignItems;
     const images = [
         {
           label: 'San Francisco – Oakland Bay Bridge, United States',
@@ -28,7 +30,14 @@ const ProjectCard = ({alignItems}) => {
             'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
         },
       ];
+      
     const projectCardRef = useRef(null);
+    const [expanded, setExpanded] = useState(false);
+
+    const handleExpandClick = () => {
+      setExpanded(!expanded);
+    };
+
 
     const [scroll, setScroll] = useState(false);
     useEffect(() => {
@@ -55,11 +64,29 @@ const ProjectCard = ({alignItems}) => {
 
     return (
       <Card ref={projectCardRef} className={scroll ? "projectCard " + alignItems : "projectCard"}>
-        <CardHeader titleTypographyProps={{variant:"h6"}} className={cardHeaderclass} title={header}/>
+        <CardHeader titleTypographyProps={{variant:"h6"}} className={cardHeaderClass} title={header}/>
         <CardContent className="projectCardContent">
             <SwipeableTextMobileStepper className={carouselClass} images={images} showControls={false} showTitle={false} />
             <Typography className="cardDescription">{description}</Typography>
         </CardContent>
+        <CardActions disableSpacing>
+          <IconButton
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse className={collapseClass} in={expanded} timeout="auto" unmountOnExit>
+          <Link href="https://github.com/Xenorsek/DeliveryDriver">
+              <img src="https://github-readme-stats.vercel.app/api/pin/?username=xenorsek&repo=deliverydriver&theme=dark" alt="github readme stats" />
+            </Link>
+        </Collapse>
+        <CardActions>
+          
+        </CardActions>
     </Card>
       )}
 export default ProjectCard;
