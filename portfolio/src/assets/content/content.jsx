408247629e98aaeaebc30dd8baa74c17b13ae9a7
Marcin <marcin.kozlowski@eengine.pl>
2025-04-25 16:14:13 +0200

    remove surname from portfolio

--- portfolio/src/assets/content/content.jsx
+++ portfolio/src/assets/content/content.jsx
@@ -15,137 +15,169 @@ import tilevania1 from "../images/tilevania1.png";
 import tilevania2 from "../images/tilevania2.png";
 import tilevania3 from "../images/tilevania3.png";
 
-export const AboutContent = "Witaj! Nazywam się Marcin Kozłowski. Jestem absolwentem studiów informatycznych. Już w technikum informatycznym rozwijałem swoje umiejętności programowania. Polubiłem szczególnie dwie technologie, którymi są .NET i React. Od 2020 roku zajmuję się projektowaniem i tworzeniem oprogramowania zawodowo.";
+export const AboutContent =
+	"Witaj! Nazywam się Marcin. Jestem absolwentem studiów informatycznych. Już w technikum informatycznym rozwijałem swoje umiejętności programowania. Polubiłem szczególnie dwie technologie, którymi są .NET i React. Od 2020 roku zajmuję się projektowaniem i tworzeniem oprogramowania zawodowo.";
 
-export const PassionContent = "Jestem osobą, która od dłuższego czasu zgłębia tajniki programowania, a moją pasją stało się tworzenie oprogramowania w technologiach .NET i React. Od 2020 roku mam przyjemność pracować zawodowo w tych dziedzinach, co pozwoliło mi rozwinąć umiejętności i zdobyć cenne doświadczenie.";
+export const PassionContent =
+	"Jestem osobą, która od dłuższego czasu zgłębia tajniki programowania, a moją pasją stało się tworzenie oprogramowania w technologiach .NET i React. Od 2020 roku mam przyjemność pracować zawodowo w tych dziedzinach, co pozwoliło mi rozwinąć umiejętności i zdobyć cenne doświadczenie.";
 
 export const reactProjects = [
-    {
-        header: "Music converter",
-        description: "Aplikacja wykorzystująca AI do konwertowania plików audio na typ danych musicSequence. Aplikacja pozwala na wczytanie pliku audio lub wyprowadzonego z mikrofonu. W aplikacji znajdziemy bibliotekę, w której istnieją przykładowe pliki muzyczne. Po utworzeniu konta możemy dodawać i polubić utwory.",
-        demoLink:"https://music-converter-6012d.firebaseapp.com/",
-        repositoryUsername: "xenorsek",
-        repositoryName: "PracowniaDyplomowa",
-        images: [
-            {
-              label: 'Strona główna',
-              imgPath: musicConverter
-            },
-            {
-              label: 'Biblioteka',
-              imgPath:musicConverter1,
-            },
-            {
-              label: 'MusicSequence',
-              imgPath:musicConverter2
-            },
-          ],
-          imageWidth:'400px',
-          imageHeight:'200px',
-    }
+	{
+		header: "Music converter",
+		description:
+			"Aplikacja wykorzystująca AI do konwertowania plików audio na typ danych musicSequence. Aplikacja pozwala na wczytanie pliku audio lub wyprowadzonego z mikrofonu. W aplikacji znajdziemy bibliotekę, w której istnieją przykładowe pliki muzyczne. Po utworzeniu konta możemy dodawać i polubić utwory.",
+		demoLink: "https://music-converter-6012d.firebaseapp.com/",
+		repositoryUsername: "xenorsek",
+		repositoryName: "PracowniaDyplomowa",
+		images: [
+			{
+				label: "Strona główna",
+				imgPath: musicConverter,
+			},
+			{
+				label: "Biblioteka",
+				imgPath: musicConverter1,
+			},
+			{
+				label: "MusicSequence",
+				imgPath: musicConverter2,
+			},
+		],
+		imageWidth: "400px",
+		imageHeight: "200px",
+	},
 ];
 
 export const netProjects = [
-  {
-    header: "UsersNotebook",
-    description: "Aplikacja UsersNotebook umożliwia zarządzanie użytkownikami: dodawanie, edycję, usuwanie oraz generowanie raportów w PDF. Zbudowana w ASP.NET Core 7 z wykorzystaniem MVC, Entity Framework oraz Docker.",
-    repositoryUserName: "xenorsek",
-    repositoryName: "UsersNotebook",
-    images: [
-      {
-        label: 'Strona główna',
-        imgPath: usersnotebook1
-      },
-      {
-        label: 'Dodaj użytkownika',
-        imgPath:usersnotebook2,
-      },
-      {
-        label: 'Raport',
-        imgPath:usersnotebook3
-      },
-    ],
-    imageWidth:'400px',
-    imageHeight:'200px',
-  }
-]
+	{
+		header: "UsersNotebook",
+		description:
+			"Aplikacja UsersNotebook umożliwia zarządzanie użytkownikami: dodawanie, edycję, usuwanie oraz generowanie raportów w PDF. Zbudowana w ASP.NET Core 7 z wykorzystaniem MVC, Entity Framework oraz Docker.",
+		repositoryUserName: "xenorsek",
+		repositoryName: "UsersNotebook",
+		images: [
+			{
+				label: "Strona główna",
+				imgPath: usersnotebook1,
+			},
+			{
+				label: "Dodaj użytkownika",
+				imgPath: usersnotebook2,
+			},
+			{
+				label: "Raport",
+				imgPath: usersnotebook3,
+			},
+		],
+		imageWidth: "400px",
+		imageHeight: "200px",
+	},
+];
 
 export const professionalProjects = [
-    {
-        header: "Developer Capacity Management Tool",
-        description: "DCMT jest systemem do głównego zarządzania kompetencjami i projektami w firmie. Aplikacja wykorzystuje .NET 6.0 + React wykorzystując Azure portal i synchronizowała się z AD. Praca była w metodologii Scrum. Właściciele projektów mogli przypisywać osoby do konkretnych stanowisk i ról w projekcie. Generowane były kody projektowe, które umożliwiały następnie rejestrowanie godzin dla pracownika. Użytkownicy otrzymywali powiadomienia o zmianach statusów ich przypisania bądź o projektach, w których się znajdowali.",
-    },
-    {
-        header: "Interview Scheduler",
-        description: "Interview Scheduler jest aplikacją webową w technologiach .NET 6.0 + Angular z serwisami Azure Portal. Praca była w metodologii Scrum. Celem aplikacji była automatyzacja procesu rekrutacji w firmie. Aplikacja umożliwiała monitorowanie procesu rekrutacji i procesu walidacji przyszłych pracowników po przez tworzenie i przypisywanie spotkania technicznego do wykwalifikowanego pracownika z firmy.",
-    },    
-    {
-      header: "CRM",
-      description: "CRM jest aplikacją łączącą kilka różnych źródłeł danych pozwalając na szybki dostęp w uporządkowanym miejscu. CRM wykorzystuje .NET 5.0 MVC z bazą Oracle. Dodatkowo wykorzystywany był DevExpress. Aplikacja umożliwiała dostęp do informacji podatników z wyszczególnieniem źrodeł danych. Pozwalała na zaawansowane filtrowanie danych, wyświetlała informację ze wskaźników biznesowych oraz umożliwiała generować raporty.",
-  }
+	{
+		header: "Developer Capacity Management Tool",
+		description:
+			"DCMT jest systemem do głównego zarządzania kompetencjami i projektami w firmie. Aplikacja wykorzystuje .NET 6.0 + React wykorzystując Azure portal i synchronizowała się z AD. Praca była w metodologii Scrum. Właściciele projektów mogli przypisywać osoby do konkretnych stanowisk i ról w projekcie. Generowane były kody projektowe, które umożliwiały następnie rejestrowanie godzin dla pracownika. Użytkownicy otrzymywali powiadomienia o zmianach statusów ich przypisania bądź o projektach, w których się znajdowali.",
+	},
+	{
+		header: "Interview Scheduler",
+		description:
+			"Interview Scheduler jest aplikacją webową w technologiach .NET 6.0 + Angular z serwisami Azure Portal. Praca była w metodologii Scrum. Celem aplikacji była automatyzacja procesu rekrutacji w firmie. Aplikacja umożliwiała monitorowanie procesu rekrutacji i procesu walidacji przyszłych pracowników po przez tworzenie i przypisywanie spotkania technicznego do wykwalifikowanego pracownika z firmy.",
+	},
+	{
+		header: "CRM",
+		description:
+			"CRM jest aplikacją łączącą kilka różnych źródłeł danych pozwalając na szybki dostęp w uporządkowanym miejscu. CRM wykorzystuje .NET 5.0 MVC z bazą Oracle. Dodatkowo wykorzystywany był DevExpress. Aplikacja umożliwiała dostęp do informacji podatników z wyszczególnieniem źrodeł danych. Pozwalała na zaawansowane filtrowanie danych, wyświetlała informację ze wskaźników biznesowych oraz umożliwiała generować raporty.",
+	},
 ];
 
 export const UnityProjects = [
-  {
-    header:"QuizUnit",
-    description:"QuizUnit - jest to gra utworzona w Unity, której przebieg gry oparty jest o Quiz. Pytania zostały dobrane tak by przypominały o podstawie tworzenia gier w Unity. Quiz składał się z 3 scen: Start - Gra - Ekran końcowy. Podczas gry użytkownik musiał odpowiedzieć na pytanie z limitem czasu. Pytania są generowane losowo co utrudnia, użytkownikowi strzelanie. Po odpowiedzeniu na wszystkie pytania, użytkownik witał się z ekranem końcowym, na którym pokazany był jego wynik, oraz przycisk umożliwiający przejście gry ponownie.",
-    repositoryUsername:"Xenorsek",
-    repositoryName:"QuizUnit",
-    images: [
-      {
-        label: 'Ekran start',
-        imgPath: quizUnit1
-      },
-      {
-        label: 'Ekran gra',
-        imgPath:quizUnit2,
-      },
-      {
-        label: 'Ekran koniec',
-        imgPath:quizUnit3
-      },
-    ],
-  },
-  {
-    header:"Snowboarder",
-    description:"Snowboarder jest grą, w której użytkownik wciela się w snowboardzistę, który zjeżdża po stoku tak by dotrzeć do mety. Musi uważać na przeszkody oraz na swoją głowę, która jest jego punktem krytycznym. Gracz może przekręcać postać i przyśpieszać podczas jazdy po śniegu tak aby uniknąć przeszkody i ukończyć stok w jak najlepszym stanie oraz o dobrym czasie.",
-    repositoryUsername:"Xenorsek",
-    repositoryName:"Snowboarder",
-    images: [
-      {
-        label: 'Snowboarder',
-        imgPath: Snowboarder1
-      },
-      {
-        label: 'Snowboarder',
-        imgPath:Snowboarder2,
-      },
-      {
-        label: 'Snowboarder',
-        imgPath:Snowboarder3
-      },
-    ],
-  },
-  {
-    header:"TileVania",
-    description:"Tilevania jest platformówką gdzie użytkownik musi uważać na wrogów, kwas i kolce. Skacz wysoko i dotrzyj do wyjścia. Przed tobą stoją 3 etapy na których będziesz musiał sprawnie poruszać się po platformach skacząc i robiąc zwariowane akrobacje. Czy jesteś na to gotowy? Pamiętaj o tym że możesz tutaj strzelać! Baw się przy tym dobrze! To najlepsza gra tego roku! Do tego ta gra jest bardzo kreatywna bo jest to platformówka, twój mózg przy tej grze pracuje na najwyższych obrotach, nie ma nic bardziej kreatywnego dla twojego samopoczucia niż to! Powodzenia!",
-    repositoryUsername:"Xenorsek",
-    repositoryName:"tilevania",
-    images: [
-      {
-        label: 'Tilevania',
-        imgPath: tilevania1
-      },
-      {
-        label: 'Tilevania',
-        imgPath:tilevania2,
-      },
-      {
-        label: 'Tilevania',
-        imgPath:tilevania3
-      },
-    ],
-  }
+	{
+		header: "QuizUnit",
+		description:
+			"QuizUnit - jest to gra utworzona w Unity, której przebieg gry oparty jest o Quiz. Pytania zostały dobrane tak by przypominały o podstawie tworzenia gier w Unity. Quiz składał się z 3 scen: Start - Gra - Ekran końcowy. Podczas gry użytkownik musiał odpowiedzieć na pytanie z limitem czasu. Pytania są generowane losowo co utrudnia, użytkownikowi strzelanie. Po odpowiedzeniu na wszystkie pytania, użytkownik witał się z ekranem końcowym, na którym pokazany był jego wynik, oraz przycisk umożliwiający przejście gry ponownie.",
+		repositoryUsername: "Xenorsek",
+		repositoryName: "QuizUnit",
+		images: [
+			{
+				label: "Ekran start",
+				imgPath: quizUnit1,
+			},
+			{
+				label: "Ekran gra",
+				imgPath: quizUnit2,
+			},
+			{
+				label: "Ekran koniec",
+				imgPath: quizUnit3,
+			},
+		],
+	},
+	{
+		header: "Snowboarder",
+		description:
+			"Snowboarder jest grą, w której użytkownik wciela się w snowboardzistę, który zjeżdża po stoku tak by dotrzeć do mety. Musi uważać na przeszkody oraz na swoją głowę, która jest jego punktem krytycznym. Gracz może przekręcać postać i przyśpieszać podczas jazdy po śniegu tak aby uniknąć przeszkody i ukończyć stok w jak najlepszym stanie oraz o dobrym czasie.",
+		repositoryUsername: "Xenorsek",
+		repositoryName: "Snowboarder",
+		images: [
+			{
+				label: "Snowboarder",
+				imgPath: Snowboarder1,
+			},
+			{
+				label: "Snowboarder",
+				imgPath: Snowboarder2,
+			},
+			{
+				label: "Snowboarder",
+				imgPath: Snowboarder3,
+			},
+		],
+	},
+	{
+		header: "TileVania",
+		description:
+			"Tilevania jest platformówką gdzie użytkownik musi uważać na wrogów, kwas i kolce. Skacz wysoko i dotrzyj do wyjścia. Przed tobą stoją 3 etapy na których będziesz musiał sprawnie poruszać się po platformach skacząc i robiąc zwariowane akrobacje. Czy jesteś na to gotowy? Pamiętaj o tym że możesz tutaj strzelać! Baw się przy tym dobrze! To najlepsza gra tego roku! Do tego ta gra jest bardzo kreatywna bo jest to platformówka, twój mózg przy tej grze pracuje na najwyższych obrotach, nie ma nic bardziej kreatywnego dla twojego samopoczucia niż to! Powodzenia!",
+		repositoryUsername: "Xenorsek",
+		repositoryName: "tilevania",
+		images: [
+			{
+				label: "Tilevania",
+				imgPath: tilevania1,
+			},
+			{
+				label: "Tilevania",
+				imgPath: tilevania2,
+			},
+			{
+				label: "Tilevania",
+				imgPath: tilevania3,
+			},
+		],
+	},
 ];
 
-export const skillsArray = [ {label:"React" , icon: <ReactIcon />}, {label:"Azure portal"}, {label:"ASP .NET Core"}, {label:"Entity FrameworkCore"}, {label:"REST"}, {label:"Linq"}, {label:"Hangfire"}, {label:"Automapper"}, {label:"Dapper"}, {label:"SOLID"}, {label:"Unit tests"}, {label:"GIT"}, {label: "Redux"}, {label:"Material UI"}, {label:"npm"}, {label:"NuGet"}, {label:"MVC"}, {label:"MySQL"}, {label:"Jira"}, {label:"Scrum"}, {label:"Agile"}];
+export const skillsArray = [
+	{ label: "React", icon: <ReactIcon /> },
+	{ label: "Azure portal" },
+	{ label: "ASP .NET Core" },
+	{ label: "Entity FrameworkCore" },
+	{ label: "REST" },
+	{ label: "Linq" },
+	{ label: "Hangfire" },
+	{ label: "Automapper" },
+	{ label: "Dapper" },
+	{ label: "SOLID" },
+	{ label: "Unit tests" },
+	{ label: "GIT" },
+	{ label: "Redux" },
+	{ label: "Material UI" },
+	{ label: "npm" },
+	{ label: "NuGet" },
+	{ label: "MVC" },
+	{ label: "MySQL" },
+	{ label: "Jira" },
+	{ label: "Scrum" },
+	{ label: "Agile" },
+];
--- portfolio/src/components/Main/Header/Header.js
+++ portfolio/src/components/Main/Header/Header.js
@@ -1,141 +1,188 @@
-import { AppBar, IconButton, Toolbar, Typography, MenuItem, Menu, Button, Switch, useMediaQuery } from '@mui/material';
-import { scroller } from 'react-scroll';
-import MenuIcon from '@mui/icons-material/Menu';
-import React, { useEffect, useState } from 'react';
-import { ColorModeContext } from '../../../App';
-const Header = ({fetchMoreData, componentsNames}) => {
-    const [darkMode, setDarkMode] = useState(false);
-    const colorMode = React.useContext(ColorModeContext);
-    const preferDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
-    
-    useEffect(()=>{
-        const darkModeFromStorage = JSON.parse(localStorage.getItem("darkmode"));
-        
-        if(darkModeFromStorage == null){
-            setDarkMode(preferDarkMode)
-        }
-        else{
-            setDarkMode(darkModeFromStorage);
-        }
-    },[preferDarkMode])
+import {
+	AppBar,
+	IconButton,
+	Toolbar,
+	Typography,
+	MenuItem,
+	Menu,
+	Button,
+	Switch,
+	useMediaQuery,
+} from "@mui/material";
+import { scroller } from "react-scroll";
+import MenuIcon from "@mui/icons-material/Menu";
+import React, { useEffect, useState } from "react";
+import { ColorModeContext } from "../../../App";
+const Header = ({ fetchMoreData, componentsNames }) => {
+	const [darkMode, setDarkMode] = useState(false);
+	const colorMode = React.useContext(ColorModeContext);
+	const preferDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
 
-    const scrollToSection = (section) => {
-        setAnchorElNavigation(null);
-        if(componentsNames[section].visible){
-            scrollToElement(componentsNames[section].name)
-        }
-        else{
-            fetchMoreData(section);
-            setTimeout(() => {
-                scrollToElement(componentsNames[section].name)
-            }, 100)
-        }
-      };
+	useEffect(() => {
+		const darkModeFromStorage = JSON.parse(localStorage.getItem("darkmode"));
 
-    const scrollToElement = (sectionName) =>{
-        const offset = 80;
-        scroller.scrollTo(sectionName, {
-            duration: 800,
-            delay: 0,
-            smooth: 'easeInOutQuart',
-            offset: -offset
-        });
-    }
+		if (darkModeFromStorage == null) {
+			setDarkMode(preferDarkMode);
+		} else {
+			setDarkMode(darkModeFromStorage);
+		}
+	}, [preferDarkMode]);
 
-    const [anchorElNavigation, setAnchorElNavigation] = React.useState(null);
-    const [anchorElPageSettings, setAnchorElPageSettings] = React.useState(null);
-    const openPageSettings = Boolean(anchorElPageSettings);
-    const openPageNavigation = Boolean(anchorElNavigation);
+	const scrollToSection = (section) => {
+		setAnchorElNavigation(null);
+		if (componentsNames[section].visible) {
+			scrollToElement(componentsNames[section].name);
+		} else {
+			fetchMoreData(section);
+			setTimeout(() => {
+				scrollToElement(componentsNames[section].name);
+			}, 100);
+		}
+	};
 
-    const handleOpenNavigation = (event) => {
-        setAnchorElNavigation(event.currentTarget);
-    };
+	const scrollToElement = (sectionName) => {
+		const offset = 80;
+		scroller.scrollTo(sectionName, {
+			duration: 800,
+			delay: 0,
+			smooth: "easeInOutQuart",
+			offset: -offset,
+		});
+	};
 
-    const handleCloseNavigation = () => {
-        setAnchorElNavigation(null);
-    };
+	const [anchorElNavigation, setAnchorElNavigation] = React.useState(null);
+	const [anchorElPageSettings, setAnchorElPageSettings] = React.useState(null);
+	const openPageSettings = Boolean(anchorElPageSettings);
+	const openPageNavigation = Boolean(anchorElNavigation);
 
-    const handleOpenPageSettings = (event) =>{
-        setAnchorElPageSettings(event.currentTarget);
-    }
+	const handleOpenNavigation = (event) => {
+		setAnchorElNavigation(event.currentTarget);
+	};
 
-    const handleClosePageSettings = () => {
-        setAnchorElPageSettings(null);
-    };
+	const handleCloseNavigation = () => {
+		setAnchorElNavigation(null);
+	};
 
-    const handleChangeDarkMode = () => {
-        setDarkMode((prev) => {
-            localStorage.setItem("darkmode", !prev);
-            return !prev;
-        });
-        colorMode.toggleColorMode();
+	const handleOpenPageSettings = (event) => {
+		setAnchorElPageSettings(event.currentTarget);
+	};
 
-    }
+	const handleClosePageSettings = () => {
+		setAnchorElPageSettings(null);
+	};
 
-return (
-    <AppBar position="sticky" className='header'>
-        <Toolbar>
-            <Typography variant="h4" component="div" sx={{ flexGrow: 1, letterSpacing:3, fontWeight:600 }}>
-                Marcin Kozłowski
-            </Typography>
-            <div className='iconNavigation'>
-                <IconButton
-                    size="large"
-                    color="inherit"
-                    aria-label="menu"
-                    aria-controls={openPageNavigation ? 'navigation-menu' : undefined}
-                    aria-haspopup="true"
-                    aria-expanded={openPageNavigation ? 'true' : undefined}
-                    onClick={handleOpenNavigation}
-                    sx={{ mr: 2 }}
-                >
-                    <MenuIcon />
-                </IconButton>
-            </div>
-            <div className="navigation">
-                <Button color="inherit" variant="text" onClick={()=>{scrollToSection(0)}}>O mnie</Button>
-                <Button color="inherit" variant="text" onClick={()=>{scrollToSection(1)}}>Doświadczenie</Button>
-                <Button color="inherit" variant="text" onClick={()=>{scrollToSection(2)}}>Projekty</Button>
-                <IconButton 
-                    size="large"
-                    edge="end"
-                    color="inherit"
-                    aria-label="menu"
-                    aria-controls={openPageSettings ? 'page-settings-menu' : undefined}
-                    aria-haspopup="true"
-                    aria-expanded={openPageSettings ? 'true' : undefined}
-                    onClick={handleOpenPageSettings}
-                    sx={{ mr: 2 }}>
-                    <MenuIcon />
-                </IconButton>
-            </div>
-        </Toolbar>
-        <Menu
-            id="navigation-menu"
-            anchorEl={anchorElNavigation}
-            open={openPageNavigation}
-            onClose={handleCloseNavigation}
-            MenuListProps={{
-            'aria-labelledby': 'basic-button',
-            }}
-        >
-            <MenuItem onClick={()=>{scrollToSection(0)}}>O mnie</MenuItem>             
-            <MenuItem onClick={()=>{scrollToSection(1)}}>Doświadczenie</MenuItem>
-            <MenuItem onClick={()=>{scrollToSection(2)}}>Projekty</MenuItem>
-            <MenuItem onClick={handleChangeDarkMode}>Dark mode <Switch checked={darkMode}/></MenuItem>  
-        </Menu>
-        <Menu
-            id="page-settings-menu"
-            anchorEl={anchorElPageSettings}
-            open={openPageSettings}
-            onClose={handleClosePageSettings}
-            MenuListProps={{
-            'aria-labelledby': 'basic-button',
-            }}
-        >
-            <MenuItem onClick={handleChangeDarkMode}>Dark mode <Switch checked={darkMode}/></MenuItem>                
-        </Menu>
-    </AppBar>
-)
-}
+	const handleChangeDarkMode = () => {
+		setDarkMode((prev) => {
+			localStorage.setItem("darkmode", !prev);
+			return !prev;
+		});
+		colorMode.toggleColorMode();
+	};
+
+	return (
+		<AppBar position="sticky" className="header">
+			<Toolbar>
+				<Typography
+					variant="h4"
+					component="div"
+					sx={{ flexGrow: 1, letterSpacing: 3, fontWeight: 600 }}>
+					Marcin
+				</Typography>
+				<div className="iconNavigation">
+					<IconButton
+						size="large"
+						color="inherit"
+						aria-label="menu"
+						aria-controls={openPageNavigation ? "navigation-menu" : undefined}
+						aria-haspopup="true"
+						aria-expanded={openPageNavigation ? "true" : undefined}
+						onClick={handleOpenNavigation}
+						sx={{ mr: 2 }}>
+						<MenuIcon />
+					</IconButton>
+				</div>
+				<div className="navigation">
+					<Button
+						color="inherit"
+						variant="text"
+						onClick={() => {
+							scrollToSection(0);
+						}}>
+						O mnie
+					</Button>
+					<Button
+						color="inherit"
+						variant="text"
+						onClick={() => {
+							scrollToSection(1);
+						}}>
+						Doświadczenie
+					</Button>
+					<Button
+						color="inherit"
+						variant="text"
+						onClick={() => {
+							scrollToSection(2);
+						}}>
+						Projekty
+					</Button>
+					<IconButton
+						size="large"
+						edge="end"
+						color="inherit"
+						aria-label="menu"
+						aria-controls={openPageSettings ? "page-settings-menu" : undefined}
+						aria-haspopup="true"
+						aria-expanded={openPageSettings ? "true" : undefined}
+						onClick={handleOpenPageSettings}
+						sx={{ mr: 2 }}>
+						<MenuIcon />
+					</IconButton>
+				</div>
+			</Toolbar>
+			<Menu
+				id="navigation-menu"
+				anchorEl={anchorElNavigation}
+				open={openPageNavigation}
+				onClose={handleCloseNavigation}
+				MenuListProps={{
+					"aria-labelledby": "basic-button",
+				}}>
+				<MenuItem
+					onClick={() => {
+						scrollToSection(0);
+					}}>
+					O mnie
+				</MenuItem>
+				<MenuItem
+					onClick={() => {
+						scrollToSection(1);
+					}}>
+					Doświadczenie
+				</MenuItem>
+				<MenuItem
+					onClick={() => {
+						scrollToSection(2);
+					}}>
+					Projekty
+				</MenuItem>
+				<MenuItem onClick={handleChangeDarkMode}>
+					Dark mode <Switch checked={darkMode} />
+				</MenuItem>
+			</Menu>
+			<Menu
+				id="page-settings-menu"
+				anchorEl={anchorElPageSettings}
+				open={openPageSettings}
+				onClose={handleClosePageSettings}
+				MenuListProps={{
+					"aria-labelledby": "basic-button",
+				}}>
+				<MenuItem onClick={handleChangeDarkMode}>
+					Dark mode <Switch checked={darkMode} />
+				</MenuItem>
+			</Menu>
+		</AppBar>
+	);
+};
 export default Header;
